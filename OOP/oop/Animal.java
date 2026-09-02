31d6e544ec2e32514ea3127aaa04167273e79f59
BananasAmIRite <jasony1230@gmail.com>
2026-09-01 21:00:37 -0400

    added instructions to Animal, corrected numbering for Dog

--- OOP/oop/Animal.java
+++ OOP/oop/Animal.java
@@ -1,5 +1,7 @@
 package OOP.oop;
 
 public class Animal {
-    
+    // 3. Complete your Animal class:
+    //    - Look at what properties & methods are shared between Cat.java and Dog.java. Add them in here
+    //    - Add a constructor that initializes each property
 }
--- OOP/oop/Dog.java
+++ OOP/oop/Dog.java
@@ -3,7 +3,7 @@ package OOP.oop;
 import OOP.oop.Animal;
 
 public class Dog {
-    // 1. Complete your Dog class:
+    // 2. Complete your Dog class:
     //    - Add the properties "name" and "age"
     //    - Add a constructor that initializes both properties
     //    - Add a makeSound() method that prints "bark"
